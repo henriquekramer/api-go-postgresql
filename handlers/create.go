e78765cd1edd09b8a2b83b7b59c6ab4b6ac868de
Henrique Kramer <henriquemkramer@gmail.com>
2023-02-27 23:07:56 -0300

    feat: add create handler

---
+++ handlers/create.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"api-postegresql/models"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	var todo models.Todo
+
+	err := json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		log.Printf("Erro ao fazer decode do json: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	id, err := models.Insert(todo)
+
+	var resp map[string]interface{}
+
+	if err != nil {
+		resp = map[string]interface{}{
+			"Error":   true,
+			"Message": fmt.Sprintf("Ocorreu um erro ao tentar inserir: %v", err),
+		}
+	} else {
+		resp = map[string]interface{}{
+			"Error":   false,
+			"Message": fmt.Sprintf("Todo inserida com sucesso! ID: %d", id),
+		}
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
