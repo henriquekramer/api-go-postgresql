cbb55397e635af091885f0911f2e8ef52e0cf2c9
Henrique Kramer <henriquemkramer@gmail.com>
2023-02-28 22:55:28 -0300

    fix bugs

--- handlers/create.go
+++ handlers/create.go
@@ -12,28 +12,29 @@ func Create(w http.ResponseWriter, r *http.Request) {
 	var todo models.Todo
 
 	err := json.NewDecoder(r.Body).Decode(&todo)
+
 	if err != nil {
-		log.Printf("Erro ao fazer decode do json: %v", err)
+		log.Printf("erro ao fazer decode do json: %v", err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
 	id, err := models.Insert(todo)
 
-	var resp map[string]interface{}
+	var resp map[string]any
 
 	if err != nil {
-		resp = map[string]interface{}{
+		resp = map[string]any{
 			"Error":   true,
 			"Message": fmt.Sprintf("Ocorreu um erro ao tentar inserir: %v", err),
 		}
 	} else {
-		resp = map[string]interface{}{
+		resp = map[string]any{
 			"Error":   false,
-			"Message": fmt.Sprintf("Todo inserida com sucesso! ID: %d", id),
+			"Message": fmt.Sprintf("Todo inserido com sucesso! ID: %d", id),
 		}
 	}
-
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(resp)
+
 }
--- models/insert.go
+++ models/insert.go
@@ -4,12 +4,13 @@ import "api-postegresql/db"
 
 func Insert(todo Todo) (id int64, err error) {
 	conn, err := db.OpenConnection()
+
 	if err != nil {
 		return
 	}
 	defer conn.Close()
 
-	sql := `INSERT INTO todos (title, description, done) VALUES ($1 $2 $3) RETURNING id`
+	sql := `INSERT INTO todos (title, description, done) VALUES ($1,$2,$3) RETURNING id`
 
 	err = conn.QueryRow(sql, todo.Title, todo.Description, todo.Done).Scan(&id)
 
